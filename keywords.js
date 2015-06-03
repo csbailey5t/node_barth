652c40913033870aa2a31ff1495abf98bbb4a051
Scott Bailey <csb5t@virginia.edu>
2015-06-03 10:46:31 -0400

    This works, but doesn't handle stopwords

--- keywords.js
+++ keywords.js
@@ -1,33 +1,55 @@
+// Based directly on this gist by Elliot Bonneville: https://gist.github.com/elliotbonneville/1bf694b8c83f358e0404
+
 var request = require('request'),
       cheerio = require('cheerio');
 
-var corpus = {},
-      totalResults = 0,
-      resultsDownloaded = 0;
+var corpus = {};
 
-function logwords () {
-    resultsDownloaded++;
+var url = "http://solomon.dkbl.alexanderstreet.com/cgi-bin/asp/philo/dkbl/getobject.pl?c.830:1.barth";
 
-    if (resultsDownloaded !== totalResults) {
-        return;
+request(url, function (error, response, body) {
+
+    // Handle errors
+    if (error) {
+        console.log("Coudn't get page because of error:" + error);
     }
+
+    // Load a page into cheerio
+    var $page = cheerio.load(body);
+    var text = $page('body').text();
+
+    // Throw away whitespace and non-alphanumeric characters
+    text = text.replace(/\s+/g, " ").replace(/[^a-zA-Z]/g, " ").toLowerCase();
+
+    // Split on spaces to list all the words on the page, then loop
+    text.split(" ").forEach(function (word) {
+
+        if (corpus[word]) {
+            // If the word is in the "corpus", increase the count by one
+            corpus[word]++;
+        } else {
+            // Otherwise, set the count for the word to one
+            corpus[word] = 1;
+        }
+
+    });
 
     var words = [];
 
-    // put all words into an array
-    for (prop in corpus) {
+    // Put all the words in an array
+    for ( var token in corpus) {
         words.push({
-            word: prop,
-            count: corpus[prop]
+            word: token,
+            count: corpus[token]
         });
     }
 
-    // sort array based on count
+    // Sort the array based on count
     words.sort(function (a, b) {
-        return b.count - a.count
+        return b.count - a.count;
     });
 
-    //log the fifty most popular words
+    // Log the first fifty most popular words
     console.log(words.slice(0, 20));
-}
+});
 
